815d4cb6aa34bbe093259cab384e85645a2d74ec
DeafKangaroo <153771887+DeafKangaroo@users.noreply.github.com>
2026-02-11 16:58:41 +0100

    endscreen info showcase 1/2

--- Assets/Scripts/WinTrigger.cs
+++ Assets/Scripts/WinTrigger.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class WinTrigger : MonoBehaviour
 {
+    public static float highScore;
+    public static float currentTime;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene("Ending");
